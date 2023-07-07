bc939ee8f36550c7e4d1529b857f4c59ad32a141
danielOuattara <daniel.ouattara@gmx.com>
2023-07-07 23:35:25 +0200

    12 ts enums

---
+++ course_12_enums/src/index.ts
@@ -0,0 +1,16 @@
+// enums
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// PascalCase
+
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Large;
+console.log(mySize);

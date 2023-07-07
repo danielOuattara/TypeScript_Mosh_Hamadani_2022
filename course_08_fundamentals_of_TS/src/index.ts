3d5d9648595ce81f4aade68f4a24614a18e114ef
danielOuattara <daniel.ouattara@gmx.com>
2023-07-07 23:34:29 +0200

    08 fundamentals of TS

---
+++ course_08_fundamentals_of_TS/src/index.ts
@@ -0,0 +1,11 @@
+// variables annotations
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+// variables auto inferred
+let sales_2 = 123_456_789;
+let course_2 = "TypeScript";
+let is_published_2 = true;
+
+let something; // type = any

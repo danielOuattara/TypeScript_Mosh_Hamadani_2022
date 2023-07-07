b94e62c7de6c079c4102212b403972c0f4268166
danielOuattara <daniel.ouattara@gmx.com>
2023-07-07 23:34:55 +0200

    10 ts arrays

---
+++ course_10_arrays/src/index.ts
@@ -0,0 +1,12 @@
+// Array
+
+let list1: number[] = [1, 2, 3]; // syntax 1
+let list2: Array<number> = [1, 2, 3]; // syntax 2
+
+//-------------------------
+let list1_2 = [1, 2, 3]; // syntax 1
+let list2_2 = [1, 2, 3]; // syntax 2
+
+// any array
+
+let numbers = []; // let numbers: any[]
